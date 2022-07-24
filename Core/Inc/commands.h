ff065b318b98878d4d234b56d8a755646a9d203b
sg200000 <saidguouihaj@gmail.com>
2022-07-24 18:32:37 +0100

    first commit

---
+++ Core/Inc/commands.h
@@ -0,0 +1,76 @@
+/*
+ * commands.h
+ *
+ *  Created on: 24‏/07‏/2022
+ *      Author: Said Guouihaj
+ */
+
+#ifndef INC_COMMANDS_H_
+#define INC_COMMANDS_H_
+
+/*
+ * @brief Structure definition of the Command type
+ */
+typedef void (*operation)(void *);
+typedef struct Command {
+	char * command;				/* This is command name */
+	operation callback;			/* This is the pointer to function that executes the command */
+
+}CommandTypeDef;
+
+/* Here you define the parameters structure if the function needs parameters to be passed */
+
+typedef enum {
+	GREEN,
+	RED
+}LEDColor;
+
+typedef enum {
+	OFF = 0,
+	ON
+}LEDState;
+
+typedef struct LED {
+	LEDColor color;
+	LEDState state;
+}LEDTypeDef;
+
+/* End function parameters data structures */
+
+/* Special characters definitions corresponding to the encoding */
+#define NEWLINE	'\n'
+#define BACKSPACE	'\177'
+// You can add more special characters here
+
+#define MAX_CHARS	100			/* Define the maximum number of characters to be received */
+#define NUM_COMMANDS	6		/* Define the number of commands you will create */
+
+extern UART_HandleTypeDef huart1;
+
+#ifdef _INCLUDE_VARIABLES
+uint8_t lastUsedPointer;
+CommandTypeDef commands[NUM_COMMANDS];			/* The array where commands are stored */
+
+/* Declare every function parameters structure variable here */
+LEDTypeDef led;
+/* End declare function parameters variables */
+
+uint8_t received[MAX_CHARS];		/* This is the received serial data buffer */
+#endif
+
+/* function protoypes for program control */
+void initializeTerminal();					/* This is executed after reset to initialize the terminal */
+void initializeCommands();					/* This is to add your command to the list of commands */
+void receiveSerialCommand();				/* This to handle terminal visualization and prepare command for handling */
+void handleSerialCommand();					/* This function is responsible of parsing the command and decide which handler to execute */
+void sendCommandResponse(char * command);
+void handleStdout(char *);					/* This is responsible for sending text in serial */
+/* End function prototypes for program control */
+
+/* Here you add function prototypes for your commands */
+void getDevInfos(void *);
+void toggleLed(void *);
+void resetDevice(void *);
+/* End commands function prototypes */
+
+#endif /* INC_COMMANDS_H_ */
---
+++ Core/Src/commands.c
@@ -0,0 +1,159 @@
+/*
+ * commands.c
+ *
+ *  Created on: 24‏/07‏/2022
+ *      Author: saidg
+ */
+#include <stdlib.h>
+#include <string.h>
+#include "stm32f4xx_hal.h"
+
+
+#define _INCLUDE_VARIABLES
+#include "commands.h"
+/*
+ * @brief	Terminal initialization Function
+ * @param	None
+ * @retval	None
+ */
+void initializeTerminal(){
+	handleStdout("\rSerial communication is established\r\n");
+}
+
+/*
+ * @brief	Commands initialization Function
+ * @param	None
+ * @retval	None
+ */
+void initializeCommands(){
+	commands[0].command = "device";
+	commands[0].callback = &getDevInfos;
+	commands[1].command = "turn on led green";
+	commands[1].callback = &toggleLed;
+	commands[2].command = "turn off led green";
+	commands[2].callback = &toggleLed;
+	commands[3].command = "turn on led red";
+	commands[3].callback = &toggleLed;
+	commands[4].command = "turn off led red";
+	commands[4].callback = &toggleLed;
+	commands[5].command = "reboot";
+	commands[5].callback = &resetDevice;
+}
+
+/*
+ * @brief	Receiving and interacting with serial data received
+ * @param	None
+ * @retval	None
+ */
+void receiveSerialCommand(){
+	int i = 0;
+	while (1){
+		while(HAL_UART_Receive(&huart1, received+i, 1, 10));
+		if (received[i]=='\r'){
+			for (uint8_t j=i;j<100;j++)
+				received[j] = '\0';
+			break;
+		}
+		else if (received[i]==BACKSPACE){
+			if (i>0){
+				HAL_UART_Transmit(&huart1, (uint8_t *)received+i, 1, 100);
+				received[i] = '\0';
+				received[i-1] = '\0';
+				i--;
+			}
+			else {
+				i = 0;
+			}
+
+		}
+		else if (received[i]>31 && received[i]<127){
+			HAL_UART_Transmit(&huart1, (uint8_t *)received+i, 1, 100);
+			i++;
+		}
+	}
+}
+/*
+ * @brief	Handle serial command and send the response
+ * @param	None
+ * @retval	None
+ */
+void handleSerialCommand(){
+	sendCommandResponse((char *)received);
+}
+
+void sendCommandResponse(char * command){
+	void * params;
+	if (!strcmp(command, commands[0].command))
+		(*commands[0].callback)(NULL);
+	else if (!strcmp(command, commands[1].command)){
+		led.color = GREEN;
+		led.state = ON;
+		params = &led;
+		(*commands[1].callback)(params);
+	}
+	else if (!strcmp(command, commands[2].command)){
+		led.color = GREEN;
+		led.state = OFF;
+		params = &led;
+		(*commands[2].callback)(params);
+	}
+	else if (!strcmp(command, commands[3].command)){
+		led.color = RED;
+		led.state = ON;
+		params = &led;
+		(*commands[3].callback)(params);
+	}
+	else if (!strcmp(command, commands[4].command)){
+		led.color = RED;
+		led.state = OFF;
+		params = &led;
+		(*commands[4].callback)(params);
+	}
+	else if (!strcmp(command, commands[5].command)){
+		(*commands[5].callback)(NULL);
+	}
+	else
+		handleStdout("\r\nOK\r\n");
+}
+
+/*
+ * @brief	Send text in serial
+ * @param	out_text : the text to be sent
+ * @retval	None
+ */
+void handleStdout(char * outText){
+	size_t outSize = strlen(outText);
+	HAL_UART_Transmit(&huart1, (uint8_t *)outText, (uint8_t)outSize, 100);
+	HAL_UART_Transmit(&huart1, (uint8_t *)"$ ", 2, 100);
+}
+
+/* Here you define your command functions*/
+
+void getDevInfos(void * ptr){
+	handleStdout("\r\nstm32\r\n");
+}
+
+void toggleLed(void * ledConf){
+	LEDTypeDef ledtemp = *(LEDTypeDef *)ledConf;
+	if (ledtemp.color == GREEN){
+		HAL_GPIO_WritePin(GPIOG,  GPIO_PIN_13, ledtemp.state);
+		if (ledtemp.state == ON)
+			handleStdout("\r\nLED Green is on\r\n");
+		else
+			handleStdout("\r\nLED Green is off\r\n");
+	}
+	else if (ledtemp.color == RED){
+		HAL_GPIO_WritePin(GPIOG,  GPIO_PIN_14, ledtemp.state);
+		if (ledtemp.state == ON)
+			handleStdout("\r\nLED Red is on\r\n");
+		else
+			handleStdout("\r\nLED Red is off\r\n");
+	}
+}
+
+void resetDevice(void * ptr){
+	handleStdout("\r\nRebooting...\r\n");
+	NVIC_SystemReset();
+}
+
+/* End define command functions */
